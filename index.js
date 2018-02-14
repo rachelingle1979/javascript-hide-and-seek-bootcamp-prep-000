943b9ffe4155d57277fc22461c6bc7f5e71f4f61
Rachel Ingle <rachelpost79@yahoo.com>
2018-02-14 20:33:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,16 +19,12 @@ function increaseRankBy(n){
   return list;
 }
 
-//function called deepest child
-//pulls out the most deeply nested child
-//id div #grand-node
-//looks along one parent-child branch, no siblings
-//test code includes document.querySelector('#grand-node div div div div'))
-
 function deepestChild(){
-  const grandNode = document.getElementById('grand-node').querySelectorAll('grand-node');
+  const grandNode = document.querySelector('#grand-node div div div div').innerHTML
   
-  return grandNode
+  
+  return grandNode;
+  
 }
 
 
