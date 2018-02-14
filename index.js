cc49d3ef8f5257fde3376e1e07ea4754bf4da79b
Rachel Ingle <rachelpost79@yahoo.com>
2018-02-14 20:26:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ function increaseRankBy(n){
 //test code includes document.querySelector('#grand-node div div div div'))
 
 function deepestChild(){
-  const grandNode = document.getElementById('grand-node').querySelectorAll('')
+  const grandNode = document.getElementById('grand-node').querySelectorAll('grand-node');
+  
+  return grandNode
 }
 
 
