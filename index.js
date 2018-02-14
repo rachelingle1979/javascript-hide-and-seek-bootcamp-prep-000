e7ea039f0edf6b288c221dc84aa03afe831839dd
Rachel Ingle <rachelpost79@yahoo.com>
2018-02-14 20:35:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function getFirstSelector(selector){
+  const firstSelector = document.querySelector(selector);
+  return firstSelector;
+}
+
+function nestedTarget(){
+  const nestedTargetEl = document.getElementById('nested');
+  return nestedTargetEl;
+}
+
+function increaseRankBy(n){
+  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+ 
+  for (let i = 0; i < list.length; i++) {
+    let num = parseInt(n);
+    list[i].innerHTML = parseInt(list[i].innerHTML) + num;
+  }
+  
+  return list;
+}
+
+function deepestChild(){
+  const grandNode = document.querySelector('#grand-node div div div div');
+  
+  return grandNode;
+  
+}
+
+
+
+
+
+
+
